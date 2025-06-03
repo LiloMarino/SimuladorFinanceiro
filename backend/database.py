bb30246ab3f80524eda5bd7c7e0f502e4379b535
LiloMarino <lilo.marino@hotmail.com>
2025-06-02 21:04:12 -0300

    feat: :sparkles: Implementado database.py com .env

---
+++ backend/database.py
@@ -0,0 +1,20 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+load_dotenv()
+
+# Prioridade: MySQL > SQLite
+mysql_url = os.getenv("MYSQL_DATABASE_URL")
+sqlite_url = os.getenv("SQLITE_DATABASE_URL", "sqlite:///./data/simulador.db")
+
+DATABASE_URL = mysql_url if mysql_url else sqlite_url
+
+engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_session() -> Session:
+    return SessionLocal()
