c79c3688dbe685e4caceaf90a7eaa1b9025122a1
LiloMarino <lilo.marino@hotmail.com>
2026-01-04 17:34:12 -0300

    fix: :wrench: Corrigido import circular

--- backend/core/runtime/user_manager.py
+++ backend/core/runtime/user_manager.py
@@ -5,7 +5,6 @@ from backend.core import repository
 from backend.core.dto.user import UserDTO
 from backend.core.logger import setup_logger
 from backend.core.utils.lazy_dict import LazyDict
-from backend.features.realtime import notify
 
 logger = setup_logger(__name__)
 
@@ -88,10 +87,14 @@ class UserManager:
 
     @classmethod
     def _emit_player_join(cls, user: UserDTO):
+        from backend.features.realtime import notify
+
         logger.info(f"Player joined: {user.nickname}")
         notify("player_join", {"nickname": user.nickname})
 
     @classmethod
     def _emit_player_exit(cls, user: UserDTO):
+        from backend.features.realtime import notify
+
         logger.info(f"Player exited: {user.nickname}")
         notify("player_exit", {"nickname": user.nickname})
