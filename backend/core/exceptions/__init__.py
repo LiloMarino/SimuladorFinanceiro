cc7763e6beddc94ae91a402365e570ff09012ea4
LiloMarino <lilo.marino@hotmail.com>
2026-01-09 22:53:46 -0300

    refactor: :recycle: Refatorado exceções para usar o FastAPI como base

--- backend/core/exceptions/__init__.py
+++ backend/core/exceptions/__init__.py
@@ -1,25 +1,25 @@
-from werkzeug.exceptions import Forbidden, Unauthorized
+from backend.core.exceptions.http_exceptions import ForbiddenError, UnauthorizedError
 
 
-class NoActiveSimulationError(Forbidden):
+class NoActiveSimulationError(ForbiddenError):
     """Não existe uma simulação ativa."""
 
     description = "Não existe uma simulação ativa"
 
 
-class SessionNotInitializedError(Unauthorized):
+class SessionNotInitializedError(UnauthorizedError):
     """Sessão do usuário não iniciada."""
 
     description = "Sessão não iniciada"
 
 
-class InsufficentCashError(Forbidden):
+class InsufficentCashError(ForbiddenError):
     """O usuário não possui saldo suficiente para completar a operação."""
 
     description = "Saldo insuficiente para completar a operação"
 
 
-class InsufficentPositionError(Forbidden):
+class InsufficentPositionError(ForbiddenError):
     """O usuário não possui posição suficiente para completar a operação."""
 
     description = "Posição insuficiente para completar a operação"
