1279886762671a09e49d21636e4bc116d12f5b53
LiloMarino <lilo.marino@hotmail.com>
2026-01-12 01:03:04 -0300

    fix: :wrench: Removido sleep cooperativo no loop da simulação

--- backend/features/simulation/simulation_loop.py
+++ backend/features/simulation/simulation_loop.py
@@ -3,8 +3,6 @@ import time
 
 from backend.core.logger import setup_logger
 from backend.core.runtime.simulation_manager import SimulationManager
-from backend.features.realtime import get_broker
-from backend.features.realtime.ws_broker import SocketBroker
 
 logger = setup_logger(__name__)
 
@@ -69,7 +67,7 @@ class SimulationLoopController:
                 speed = simulation.get_speed()
 
                 if speed <= 0:
-                    self._sleep(0.1)
+                    time.sleep(0.1)
                     continue
 
                 try:
@@ -79,21 +77,11 @@ class SimulationLoopController:
                     SimulationManager.clear_simulation()
                     break
 
-                self._sleep(1 / speed)
+                time.sleep(1 / speed)
 
         except Exception:
             logger.critical("Erro fatal no loop da simulação.")
             logger.exception("Erro fatal no loop da simulação.")
-
-    # --------------------------------------------------
-    # 💤 Sleep compatível com WS / SSE
-    # --------------------------------------------------
-    def _sleep(self, delay: float):
-        broker = get_broker()
-        if isinstance(broker, SocketBroker):
-            broker.socketio.sleep(delay)  # type: ignore
-        else:
-            time.sleep(delay)
 
 
 # --------------------------------------------------
